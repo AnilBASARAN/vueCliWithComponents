ef81b82dd9041e679a0f63536a5cbb2293afaa27
AnilBASARAN <nlbsrn@gmail.com>
2025-05-27 20:34:11 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+     <h2>My Friends</h2>
+   <ul>
+    <friend-contact></friend-contact>
+    <friend-contact></friend-contact>
+   </ul>
+  </section>
+</template>
+
+<script>
+import FriendContact from './components/FriendContact.vue'
+ export default  {
+  components: { FriendContact },
+    data(){
+        return {
+            friends:[
+                {id:"manuel",name:"Manuel Lorenz",phone:"01234545",email:"yowyow@"},
+                {id:"jack",name:"Jack Jones",phone:"555",email:"heyhey@"},
+            ]
+        }
+    }
+}
+</script>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,27 @@
+<template>
+    <li>
+        <h2>{{ friend.name }}</h2>
+        <button @click="toggleDetails" >Show Details</button>
+        <ul v-if="detailsAreVisible" >
+            <li><strong>Phone:</strong>{{ friend.phone }}</li>
+            <li><strong>Email:</strong>{{ friend.email }}</li>
+        </ul>
+    </li>
+</template>
+
+<script>
+export default {
+    data(){
+        return {
+            detailsAreVisible:true,
+            friend: {id:"jack",name:"Jack Jones",phone:"555",email:"heyhey@"}
+        }
+    },
+    methods: {
+        toggleDetails(){
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+}
+
+</script>
